7dbae0d2c8af64f165c4971ea51035a461e1e9aa
Andrii Verbytskyi <you@example.com>
2025-02-05 16:35:00 +0100

    OK

---
+++ panel/indico_panel/__init__.py
@@ -0,0 +1,19 @@
+# This file is part of the MPP Indico plugins.
+# Copyright (C) 2025 MPP
+#
+# The MPP Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License; see
+# the LICENSE file for more details.
+
+from celery.schedules import crontab
+
+from indico.core.celery import celery
+from indico.util.i18n import make_bound_gettext
+
+
+_ = make_bound_gettext('panel')
+
+
+@celery.periodic_task(run_every=crontab(minute='*/15'))
+def scheduled_update():
+    printf("Hello world")
